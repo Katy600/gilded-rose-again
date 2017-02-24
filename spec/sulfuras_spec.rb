11eb9bc155322ed6c4dd0b778ff43872901b6e13
Katy McCann <katy.alice.mccann@gmail.com>
2017-02-24 16:19:56 +0000

    No code added but past test for quality stays the same

--- spec/sulfuras_spec.rb
+++ spec/sulfuras_spec.rb
@@ -1,8 +1,13 @@
 require 'sulfuras'
 
 describe Sulfuras do
-  it "'Sulfuras, Hand of Ragnaros', sell_in does not change" do
+  it "sell_in does not change" do
     item = Sulfuras.new("Sulfuras, Hand of Ragnaros", 10, 50)
     expect {item.update_quality()}.not_to change {item.sell_in}
+  end
+
+  it "Quality does not change" do
+    item = Sulfuras.new("Sulfuras, Hand of Ragnaros", 10, 50)
+    expect {item.update_quality()}.not_to change {item.quality}
   end
 end
