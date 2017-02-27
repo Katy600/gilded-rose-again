bd39a52cc574b9ef989ada7ec0dcd73fe0e1e2e5
Katy McCann <katy.alice.mccann@gmail.com>
2017-02-27 13:16:25 +0000

    Refactor BackStage pass into a case when statement and create smaller method within that

--- lib/backstage_pass.rb
+++ lib/backstage_pass.rb
@@ -5,22 +5,41 @@ class BackStagePass < Item
   SELL_IN_REDUCTION = 1
 
   def update_quality
-    if @sell_in > 10
-       @quality += 1
-      sell_in_decreases_by_one
-    elsif (@sell_in <= 10) and (@sell_in > 5)
-      @quality += 2
-      sell_in_decreases_by_one
-    elsif (@sell_in <= 5) and (@sell_in > 0)
-      @quality += 3
-      sell_in_decreases_by_one
+    case
+      when sell_in_is_greater_than_ten  then  addOne and decreases_sell_in_by_one
+      when sell_in_is_ten_or_less       then  addTwo and decreases_sell_in_by_one
+      when sell_in_is_five_or_less      then  addThree and decreases_sell_in_by_one
     else
-       @quality = 0
+      @quality = 0
     end
   end
 
-  def sell_in_decreases_by_one
+  def sell_in_is_greater_than_ten
+    @sell_in > 10
+  end
+
+  def sell_in_is_ten_or_less
+    (@sell_in <= 10) and (@sell_in > 5)
+  end
+
+  def sell_in_is_five_or_less
+     (@sell_in <= 5) and (@sell_in > 0)
+  end
+
+  def decreases_sell_in_by_one
     @sell_in -= SELL_IN_REDUCTION
+  end
+
+  def addOne
+    @quality += 1
+  end
+
+  def addTwo
+    @quality += 2
+  end
+
+  def addThree
+    @quality += 3
   end
 
 end
